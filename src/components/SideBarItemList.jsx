2587aa76d9796123a0f08ee05ae4f8eefcc43c74
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-09-09 23:43:59 +0700

    refactor: reduce code complex and reduce unneccessary render by remove some hooks, components and HOC

--- src/components/SideBarItemList.jsx
+++ src/components/SideBarItemList.jsx
@@ -1,5 +1,3 @@
-import { useCallback, useMemo, useState } from 'react';
-
 import HomeIcon from '@mui/icons-material/Home';
 import InventoryIcon from '@mui/icons-material/Inventory';
 import ListAltIcon from '@mui/icons-material/ListAlt';
@@ -9,63 +7,60 @@ import SideBarItem from './SideBarItem';
 import RedeemIcon from '@mui/icons-material/Redeem';
 
 const SideBarItemList = () => {
-  const itemList = useMemo(
-    () => [
-      {
-        icon: <HomeIcon />,
-        title: 'Trang chủ',
-        label: 'home',
-        childItems: [],
-      },
-      {
-        icon: <InventoryIcon />,
-        title: 'Quản lý Sản phẩm',
-        label: 'product',
-        childItems: [
-          'Ưu đãi',
-          'Giảm giá',
-          'Nhãn hàng',
-          'Loại thông số',
-          'Loại sản phẩm',
-        ],
-      },
-      {
-        icon: <ListAltIcon />,
-        title: 'Quản lý Đơn hàng',
-        label: 'order',
-        childItems: ['Trạng thái', 'Thanh toán', 'Vận chuyển'],
-      },
-      {
-        icon: <PeopleIcon />,
-        title: 'Quản lý Người dùng',
-        label: 'user',
-        childItems: [],
-      },
-      {
-        icon: <RedeemIcon />,
-        title: 'Voucher',
-        label: 'voucher',
-        childItems: [],
-      },
-      {
-        icon: <SettingsIcon />,
-        title: 'Cấu hình hệ thống',
-        label: 'settings',
-        childItems: [],
-      },
-    ],
-    []
-  );
+  const itemList = [
+    {
+      icon: <HomeIcon />,
+      title: 'Trang chủ',
+      label: 'home',
+      childItems: [],
+    },
+    {
+      icon: <InventoryIcon />,
+      title: 'Quản lý Sản phẩm',
+      label: 'product',
+      childItems: [
+        'Ưu đãi',
+        'Giảm giá',
+        'Nhãn hàng',
+        'Loại thông số',
+        'Loại sản phẩm',
+      ],
+    },
+    {
+      icon: <ListAltIcon />,
+      title: 'Quản lý Đơn hàng',
+      label: 'order',
+      childItems: ['Trạng thái', 'Thanh toán', 'Vận chuyển'],
+    },
+    {
+      icon: <PeopleIcon />,
+      title: 'Quản lý Người dùng',
+      label: 'user',
+      childItems: [],
+    },
+    {
+      icon: <RedeemIcon />,
+      title: 'Voucher',
+      label: 'voucher',
+      childItems: [],
+    },
+    {
+      icon: <SettingsIcon />,
+      title: 'Cấu hình hệ thống',
+      label: 'settings',
+      childItems: [],
+    },
+  ];
 
   const [isToggled, setIsToggled] = useState({});
 
-  const handleClickedItem = useCallback((label) => {
+  const handleClickedItem = (label) => {
     label !== '' &&
       setIsToggled((prevState) => ({
         ...prevState,
         [label]: !prevState[label],
       }));
-  }, []);
+  };
 
   return (
     <div className='overflow-auto h-3/4 mt-4 w-full pr-6 pl-4 no-scrollbar'>
