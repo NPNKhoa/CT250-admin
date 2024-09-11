5a6e472022da95855c21ca8fa4d7c951704b27ed
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-09-12 05:40:07 +0700

    refactor: optimize UI/UX by show the current page on sidebar item

--- src/components/SideBarItem.jsx
+++ src/components/SideBarItem.jsx
@@ -2,14 +2,24 @@ import PropTypes from 'prop-types';
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const SideBarItem = ({ icon, label, path, childItems, isToggled, onClick }) => {
+  const currentPath = useLocation().pathname;
+
+  // prevent the child list shrink when move from a child item to parent item
+  if (isToggled === true && currentPath !== path) {
+    onClick = () => {};
+  }
+
   return (
     <>
       <Link
         to={path}
-        className='flex justify-between items-center hover:cursor-pointer hover:bg-slate-300 rounded-md pr-2'
+        className={`flex justify-between items-center hover:cursor-pointer hover:bg-slate-300 rounded-md pr-2 ${
+          currentPath === path &&
+          'text-white font-semibold bg-[#e95221] hover:bg-[#e78212]'
+        }`}
         onClick={() => onClick()}
       >
         <div className='flex justify-start items-center gap-4 rounded-2xl p-4'>
@@ -22,11 +32,14 @@ const SideBarItem = ({ icon, label, path, childItems, isToggled, onClick }) => {
 
       {isToggled && (
         <div>
-          {childItems.map(({ childId, label, path }) => (
+          {childItems.map(({ childId, label, path: childPath }) => (
             <Link
               key={childId}
-              to={path}
-              className='p-4 block hover:cursor-pointer hover:bg-slate-300 rounded-md ps-14'
+              to={childPath}
+              className={`p-4 block hover:cursor-pointer hover:bg-slate-300 rounded-md ps-14 ${
+                currentPath === childPath &&
+                'text-white font-semibold bg-[#e95221] hover:bg-[#e78212]'
+              }`}
             >
               {label}
             </Link>
