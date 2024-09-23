e6e60d3b41bf48333712858f4b1d49670f41fd42
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-09-23 13:13:22 +0700

    refactor: configure theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { ThemeProvider } from '@emotion/react';
+import theme from './configs/theme/theme.js';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+);
