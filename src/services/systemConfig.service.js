623b0707c7a90a8dfb43f4db7daeeca3b60a5524
NPNKhoa <khoa.nphucnguyen@gmail.com>
2024-10-02 14:10:29 +0700

    fix: fix SystemConfigService configs

--- src/services/systemConfig.service.js
+++ src/services/systemConfig.service.js
@@ -1,12 +1,13 @@
-import apiService from './api.service';
+import ApiService from './api.service';
 class SystemConfigService {
   constructor(path = '/system-configurations') {
     this.path = path;
+    this.api = new ApiService(`${import.meta.env.VITE_API_BASE_URL}${path}`);
   }
 
   async getConfig() {
     try {
-      return await apiService.get(`${this.path}/current`);
+      return await this.api.get(`/current`);
     } catch (error) {
       console.log(error?.message || error);
       throw error;
