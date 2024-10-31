4128990c4ab897aa1b137a3b49eefd4232807a88
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-10-31 21:52:31 +0700

    hide user and setting page with staff

--- src/configs/sidebarElements.jsx
+++ src/configs/sidebarElements.jsx
@@ -1,12 +1,9 @@
 import { v4 as uuidv4 } from 'uuid';
-
 import HomeIcon from '@mui/icons-material/Home';
 import InventoryIcon from '@mui/icons-material/Inventory';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import PeopleIcon from '@mui/icons-material/People';
 import SettingsIcon from '@mui/icons-material/Settings';
-import AccessibilityIcon from '@mui/icons-material/Accessibility';
-
 import {
   BrandPage,
   DiscountPage,
@@ -22,95 +19,110 @@ import {
   ServicePage,
   CategoryPage,
 } from '../pages';
+import { useSelector } from 'react-redux';
 
-export default [
-  {
-    id: uuidv4(),
-    path: '/',
-    element: <HomePage />,
-    icon: <HomeIcon />,
-    label: 'Trang chủ',
-    childItems: [],
-  },
-  {
-    id: uuidv4(),
-    path: '/product',
-    element: <ProductPage />,
-    icon: <InventoryIcon />,
-    label: 'Quản lý Sản phẩm',
-    childItems: [
+// Tạo component để lấy role người dùng và định nghĩa routes
+const useRoutes = () => {
+  const userRole = useSelector((state) => state.users?.user?.role?.role);
+
+  const routes = [
+    {
+      id: uuidv4(),
+      path: '/',
+      element: <HomePage />,
+      icon: <HomeIcon />,
+      label: 'Trang chủ',
+      childItems: [],
+    },
+    {
+      id: uuidv4(),
+      path: '/product',
+      element: <ProductPage />,
+      icon: <InventoryIcon />,
+      label: 'Quản lý Sản phẩm',
+      childItems: [
+        {
+          childId: `${uuidv4()}-child`,
+          label: 'Giảm giá',
+          path: '/discount',
+          element: <DiscountPage />,
+        },
+        {
+          childId: `${uuidv4()}-child`,
+          label: 'Ưu đãi',
+          path: '/promotion',
+          element: <PromotionPage />,
+        },
+        {
+          childId: `${uuidv4()}-child`,
+          label: 'Dịch vụ',
+          path: '/service',
+          element: <ServicePage />,
+        },
+        {
+          childId: `${uuidv4()}-child`,
+          label: 'Voucher',
+          path: '/voucher',
+          element: <VoucherPage />,
+        },
+        {
+          childId: `${uuidv4()}-child`,
+          label: 'Danh mục',
+          path: '/category',
+          element: <CategoryPage />,
+        },
+        {
+          childId: `${uuidv4()}-child`,
+          label: 'Thương hiệu',
+          path: '/brand',
+          element: <BrandPage />,
+        },
+        {
+          childId: `${uuidv4()}-child`,
+          label: 'Loại thông số',
+          path: '/specification',
+          element: <SpecificationPage />,
+        },
+        {
+          childId: `${uuidv4()}-child`,
+          label: 'Loại sản phẩm',
+          path: '/product-type',
+          element: <ProductTypePage />,
+        },
+      ],
+    },
+    {
+      id: uuidv4(),
+      path: '/order',
+      element: <OrderPage />,
+      icon: <ListAltIcon />,
+      label: 'Quản lý Đơn hàng',
+      childItems: [],
+    },
+  ];
+
+  if (userRole === 'admin') {
+    routes.push(
       {
-        childId: `${uuidv4()}-child`,
-        label: 'Giảm giá',
-        path: '/discount',
-        element: <DiscountPage />,
+        id: uuidv4(),
+        path: '/user',
+        element: <UserPage />,
+        icon: <PeopleIcon />,
+        label: 'Quản lý Người dùng',
+        childItems: [],
       },
       {
-        childId: `${uuidv4()}-child`,
-        label: 'Ưu đãi',
-        path: '/promotion',
-        element: <PromotionPage />,
+        id: uuidv4(),
+        path: '/settings',
+        element: <SettingPageWrapper />,
+        icon: <SettingsIcon />,
+        label: 'Cấu hình hệ thống',
+        childItems: [],
       },
-      {
-        childId: `${uuidv4()}-child`,
-        label: 'Dịch vụ',
-        path: '/service',
-        element: <ServicePage />,
-      },
-      {
-        childId: `${uuidv4()}-child`,
-        label: 'Voucher',
-        path: '/voucher',
-        element: <VoucherPage />,
-      },
-      {
-        childId: `${uuidv4()}-child`,
-        label: 'Danh mục',
-        path: '/category',
-        element: <CategoryPage />,
-      },
-      {
-        childId: `${uuidv4()}-child`,
-        label: 'Thương hiệu',
-        path: '/brand',
-        element: <BrandPage />,
-      },
-      {
-        childId: `${uuidv4()}-child`,
-        label: 'Loại thông số',
-        path: '/specification',
-        element: <SpecificationPage />,
-      },
-      {
-        childId: `${uuidv4()}-child`,
-        label: 'Loại sản phẩm',
-        path: '/product-type',
-        element: <ProductTypePage />,
-      },
-    ],
-  },
-  {
-    id: uuidv4(),
-    path: '/order',
-    element: <OrderPage />,
-    icon: <ListAltIcon />,
-    label: 'Quản lý Đơn hàng',
-    childItems: [],
-  },
-  {
-    id: uuidv4(),
-    path: '/user',
-    element: <UserPage />,
-    icon: <PeopleIcon />,
-    label: 'Quản lý Người dùng',
-    childItems: [],
-  },
-  {
-    id: uuidv4(),
-    path: '/settings',
-    element: <SettingPageWrapper />,
-    icon: <SettingsIcon />,
-    label: 'Cấu hình hệ thống',
-    childItems: [],
-  },
-];
+    );
+  }
+
+  return routes;
+};
+
+export default useRoutes;
