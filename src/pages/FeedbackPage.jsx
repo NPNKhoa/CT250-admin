c19e359cc2897818a40876576c3e920a2d3d0478
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-11-06 21:55:59 +0700

    get data and reply email

--- src/pages/FeedbackPage.jsx
+++ src/pages/FeedbackPage.jsx
@@ -1,10 +1,11 @@
 import { Fragment, useEffect, useMemo, useState } from 'react';
-import { IconButton, Button, Modal } from '@mui/material';
-import { RemoveRedEye } from '@mui/icons-material';
+import { IconButton, Button, Modal, TextField } from '@mui/material';
+import { RemoveRedEye, Send } from '@mui/icons-material';
 import ActionHeader from '../components/common/ActionHeader';
 import TableComponent from '../components/common/TableComponent';
 import AlertDialog from '../components/common/AlertDialog';
 import feedbackService from '../services/feedback.service';
+import { toast } from 'react-toastify';
 
 const FeedbackPage = () => {
   const [feedbacks, setFeedbacks] = useState([]);
@@ -12,6 +13,9 @@ const FeedbackPage = () => {
   const [editData, setEditData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  const [isReplying, setIsReplying] = useState(false);
+  const [replyText, setReplyText] = useState('');
 
   const [alertConfig, setAlertConfig] = useState({
     open: false,
@@ -52,6 +56,19 @@ const FeedbackPage = () => {
   const handleEdit = (feedback) => {
     setEditData(feedback);
     setModalOpen(true);
+  };
+
+  const handleSubmit = async (feedbackId, answer) => {
+    try {
+      await feedbackService.replyEmail(feedbackId, answer);
+      setReplyText(''); // Đặt lại nội dung trả lời
+      setIsReplying(false);
+      fetchFeebbacks();
+      toast.success('Đã trả lời phản hồi');
+    } catch (error) {
+      toast.error('Lỗi khi trả lời phản hồi');
+      console.log(error);
+    }
   };
 
   const rows = useMemo(
@@ -186,10 +203,19 @@ const FeedbackPage = () => {
               X
             </Button>
           </div>
-
-          <div className="flex items-center gap-2">
-            {editData?.senderName}
+          <div className="items-center gap-2">
             <div className="flex flex-col space-y-2">
+              <div className="flex justify-between">
+                <div className="flex gap-3 rounded-md border border-gray-300 p-2 font-medium">
+                  Người gửi: {editData?.senderName || 'Chưa có thông tin'}
+                </div>
+                <div className="flex gap-3 rounded-md border border-gray-300 p-2 font-medium">
+                  Ngày tạo:{' '}
+                  {editData?.createdAt
+                    ? new Date(editData.createdAt).toLocaleDateString('vi-VN')
+                    : 'Chưa có thông tin'}
+                </div>
+              </div>
               <div className="flex gap-3 rounded-md border border-gray-300 p-2 font-medium">
                 Email: {editData?.senderEmail || 'Chưa có thông tin'}
               </div>
@@ -201,26 +227,69 @@ const FeedbackPage = () => {
               <div className="flex gap-3 rounded-md border border-gray-300 p-2 font-medium">
                 Nội dung: {editData?.question || 'Chưa có thông tin'}
               </div>
-
-              <div className="flex gap-3 rounded-md border border-gray-300 p-2 font-medium">
-                Ngày tạo:{' '}
-                {editData?.createdAt
-                  ? new Date(editData.createdAt).toLocaleDateString('vi-VN')
-                  : 'Chưa có thông tin'}
-              </div>
             </div>
           </div>
 
-          <div> </div>
+          <div>
+            {' '}
+            {!editData?.answer == '' && (
+              <div className="my-3 min-h-[80px] rounded-md border border-gray-300 p-2 font-medium">
+                <h3 className="text-lg font-semibold">Đã trả lời:</h3>
+                <div className="flex-col gap-3 rounded-md border border-gray-300 p-2 font-medium">
+                  <div className="flex justify-between">
+                    <br />
+                  </div>
+                  Nội dung: {editData?.answer || 'Chưa có thông tin'}
+                </div>
+              </div>
+            )}
+          </div>
+
+          {isReplying && (
+            <>
+              <div className="mt-2 flex items-center gap-2">
+                <TextField
+                  fullWidth
+                  multiline
+                  rows={2}
+                  variant="outlined"
+                  placeholder="Nhập câu trả lời của bạn"
+                  value={replyText}
+                  onChange={(e) => setReplyText(e.target.value)}
+                  className="flex-1" // Để nút có thể linh hoạt với chiều rộng
+                  InputProps={{
+                    style: {
+                      borderRadius: '8px', // Bo góc cho TextField
+                      paddingRight: '50px', // Để không bị chèn nút
+                    },
+                  }}
+                />
+                <Button
+                  color="primary"
+                  variant="contained"
+                  onClick={() => {
+                    handleSubmit(editData._id, replyText);
+                  }}
+                  style={{
+                    borderRadius: '8px', // Bo góc cho nút
+                    padding: '8px 16px', // Thêm padding cho nút
+                    minWidth: '50px',
+                  }}
+                  startIcon={<Send />}
+                >
+                  Gửi
+                </Button>
+              </div>
+            </>
+          )}
 
           <div className="mt-3 flex justify-end gap-4">
             <Button
               color="primary"
               variant="outlined"
-              onClick={() => {}}
-            ></Button>
-            <Button variant="outlined" color="primary" onClick={() => {}}>
-              Xóa
+              onClick={() => setIsReplying(!isReplying)}
+            >
+              {isReplying ? 'Hủy' : 'Trả lời'}
             </Button>
           </div>
         </div>
